70372f708e3448120667a42ae99763215bb3f7f8
bpames <bpames@ua.edu>
2019-05-16 16:50:53 -0500

    Draft R code for Nupdate.

--- R/penzda.R
+++ R/penzda.R
@@ -394,3 +394,31 @@ predict <- function(obj, Xtest, Ytest){
   # Output.
   return(list(mc = mcrate, l0 = l0, preds = preds, cent = cent, dist = dist))
 }
+
+
+#' Nupdate1 - Updates null basis using single Householder reflection.
+#'
+#' @param N matrix whose columns are orthonormal basis for null space of within-class covariance matrix W.
+#' @param v new vector
+#' @return N1 matrix whose columns are orthonormal basis for intersection of col(N) and orthogonal complement of v. 
+#' @export
+
+Nupdate <- function(N,v){
+  # Extract column of partial QR factorization (used to find N) to reflect
+  # onto x-axis.
+  x <- t(N) %*% v
+  
+  # dimension of current null-space.
+  d <- length(x)
+  
+  # Update x to (implicitly) calculate Householder reflector.
+  x[1] <- sign(x[1])*norm(x=x, type = 'F') + x[1];
+  x <- x/norm(x=x, type = 'F');
+  
+  # Calculate N1 using Householder reflection.
+  N1 <- N[, 2:d] - 2* (N %*% x) %*% t(x[2:d])
+  
+  # Output.
+  return(N1)
+  
+}
